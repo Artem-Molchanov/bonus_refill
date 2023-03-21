e948a3227c0fa339e1798702a4678a96c31d7311
Artem Molchanov <artem700@gmail.com>
2023-03-21 15:01:05 -0300

    add ALL

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int currentAccountSum = 100;
+        int addCash = 1100;
+        int bonus;
+
+        if(addCash > 1000) {
+            bonus = addCash / 100;
+        } else {
+            bonus = 0;
+        }
+        int totalSum = currentAccountSum + addCash + bonus;
+
+        System.out.println(totalSum);
+
+    }
+
+}
